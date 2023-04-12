111bc0934b113c9e7d66ab9d69d8d823fde1bbcf
FRambhadjan <302881506@student.rocmondriaan.nl>
2023-04-12 16:17:21 +0200

    volgende les af

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+
+let profile = {
+    email : "302881506@student.rocmondriaan.nl",
+    userName : "Frambhadjan",
+    bornDate : "2 December 2006",
+    profilePicture : "",
+    bio : "",
+    projecten : "",
+    education : "",
+    skills : "",
+    bannerColour : "Red",
+    socialMedias : ["YouTube", "Instagram", "Facebook"]
+}
+
